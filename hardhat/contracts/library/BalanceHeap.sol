87390293b181de773145be54b079b8c49301a12e
Luca Rizzo <lucalucasrizzo@gmail.com>
2025-06-04 17:55:00 +0200

    using minheap

---
+++ hardhat/contracts/library/BalanceHeap.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "../struct/Balance.sol";
+
+library BalanceHeap {
+    struct Heap {
+        Balance[] data;
+        uint256 size;
+        bool isMinHeap;
+    }
+
+    function initMinHeap(
+        uint256 maxElements
+    ) internal pure returns (Heap memory) {
+        return
+            Heap({data: new Balance[](maxElements), size: 0, isMinHeap: true});
+    }
+
+    function initMaxHeap(
+        uint256 maxElements
+    ) internal pure returns (Heap memory) {
+        return
+            Heap({data: new Balance[](maxElements), size: 0, isMinHeap: false});
+    }
+
+    function insert(Heap memory heap, Balance memory value) internal pure {
+        require(heap.size < heap.data.length, "Heap full");
+        heap.data[heap.size] = value;
+        siftUp(heap, heap.size);
+        heap.size++;
+    }
+
+    function extractTop(
+        Heap memory heap
+    ) internal pure returns (Balance memory) {
+        require(heap.size > 0, "Heap is empty");
+        Balance memory top = heap.data[0];
+        heap.size--;
+        heap.data[0] = heap.data[heap.size];
+        siftDown(heap, 0);
+        return top;
+    }
+
+    function currentSize(Heap memory heap) internal pure returns (uint256) {
+        return heap.size;
+    }
+
+    function compare(
+        Heap memory heap,
+        Balance memory a,
+        Balance memory b
+    ) private pure returns (bool) {
+        return heap.isMinHeap ? a.amount < b.amount : a.amount > b.amount;
+    }
+
+    function siftUp(Heap memory heap, uint256 index) private pure {
+        while (index > 0) {
+            uint256 parent = (index - 1) / 2;
+
+            if (!compare(heap, heap.data[index], heap.data[parent])) {
+                break;
+            }
+
+            (heap.data[index], heap.data[parent]) = (
+                heap.data[parent],
+                heap.data[index]
+            );
+
+            index = parent;
+        }
+    }
+
+    function siftDown(Heap memory heap, uint256 index) private pure {
+        uint256 length = heap.size;
+        uint best = bestBetweenIndexAndChildren(heap, length, index);
+        while (best != index) {
+            (heap.data[index], heap.data[best]) = (
+                heap.data[best],
+                heap.data[index]
+            );
+            index = best;
+            best = bestBetweenIndexAndChildren(heap, length, index);
+        }
+    }
+
+    function bestBetweenIndexAndChildren(
+        Heap memory heap,
+        uint256 size,
+        uint256 index
+    ) private pure returns (uint256) {
+        uint256 left = 2 * index + 1;
+        uint256 right = 2 * index + 2;
+        uint256 smallest = index;
+        if (
+            left < size && compare(heap, heap.data[left], heap.data[smallest])
+        ) {
+            smallest = left;
+        }
+        if (
+            right < size && compare(heap, heap.data[right], heap.data[smallest])
+        ) {
+            smallest = right;
+        }
+
+        return smallest;
+    }
+}
--- hardhat/contracts/library/DebtSimplifier.sol
+++ hardhat/contracts/library/DebtSimplifier.sol
@@ -4,6 +4,7 @@ import "../struct/Expense.sol";
 import "../struct/Group.sol";
 import "../struct/Balance.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "./BalanceHeap.sol";
 
 using EnumerableSet for EnumerableSet.AddressSet;
 
@@ -15,61 +16,49 @@ library DebtSimplifier {
     );
 
     function simplifyDebt(Group storage group) internal {
-        // To work in memory and simplify future steps, both 'debitors' and 'creditors' arrays are preallocated with the full number of group members.
-        // Only the first 'debtSize' and 'credSize' elements are actually used, since Solidity does not allow dynamic resizing (e.g. push) in memory arrays.
-        (
-            Balance[] memory debitors,
-            uint debtSize,
-            Balance[] memory creditors,
-            uint credSize
-        ) = extractSortedCreditorsAndDebitors(group);
-        resetDebtGraph(group);
-        while (credSize > 0 && debtSize > 0) {
-            Balance memory largestCreditor = creditors[--credSize];
-            Balance memory largestDebitor = debitors[--debtSize];
-            uint256 largestCreditorAmount = abs(largestCreditor.amount);
-            uint256 largestDebitorAmount = abs(largestDebitor.amount);
-            uint256 minValue = min(largestCreditorAmount, largestDebitorAmount);
-            group.debts[largestDebitor.member][
-                largestCreditor.member
-            ] = minValue;
-            //positive amount means that previous largest creditor still needs to receive some money
-            //instead negative amount means that previous largest debitor still needs to give some money
-            int256 toAdjust = largestCreditor.amount + largestDebitor.amount;
-            if (toAdjust < 0) {
-                largestDebitor.amount = toAdjust;
-                debitors[debtSize++] = largestDebitor;
-                correctLastElementPosition(debitors, debtSize);
-            } else if (largestDebitorAmount < largestCreditorAmount) {
-                largestCreditor.amount = toAdjust;
-                creditors[credSize++] = largestCreditor;
-                correctLastElementPosition(creditors, credSize);
-            }
-        }
-        emit DebtSimplified(group.id);
-    }
-
-    function extractSortedCreditorsAndDebitors(
-        Group storage group
-    ) private view returns (Balance[] memory, uint, Balance[] memory, uint) {
+        // To work in memory and simplify future steps, we use two heaps:
+        // - A min-heap for debitors (negative balances)
+        // - A max-heap for creditors (positive balances)
+        // This allows efficient extraction of the largest creditor and debitor at each step.
         uint memberCount = group.members.length();
-        Balance[] memory creditors = new Balance[](memberCount);
-        Balance[] memory debitors = new Balance[](memberCount);
-        uint credSize = 0;
-        uint debtSize = 0;
+        BalanceHeap.Heap memory debitors = BalanceHeap.initMinHeap(memberCount);
+        BalanceHeap.Heap memory creditors = BalanceHeap.initMaxHeap(memberCount);
 
         for (uint i = 0; i < memberCount; i++) {
             address member = group.members.at(i);
             int256 balanceAmount = group.balances[member];
             if (balanceAmount > 0) {
-                creditors[credSize++] = Balance(member, balanceAmount);
+                BalanceHeap.insert(creditors, Balance(member, balanceAmount));
             } else if (balanceAmount < 0) {
-                debitors[debtSize++] = Balance(member, balanceAmount);
+                BalanceHeap.insert(debitors, Balance(member, balanceAmount));
             }
         }
-        insertionSort(creditors, credSize);
-        insertionSort(debitors, debtSize);
-        return (debitors, debtSize, creditors, credSize);
+
+        resetDebtGraph(group);
+
+        while (creditors.size > 0 && debitors.size > 0) {
+            // Extract the largest creditor and debitor
+            Balance memory largestCreditor = BalanceHeap.extractTop(creditors);
+            Balance memory largestDebitor = BalanceHeap.extractTop(debitors);
+
+            uint256 largestCreditorAmount = abs(largestCreditor.amount);
+            uint256 largestDebitorAmount = abs(largestDebitor.amount);
+            uint256 minValue = min(largestCreditorAmount, largestDebitorAmount);
+
+            group.debts[largestDebitor.member][largestCreditor.member] = minValue;
+
+            // positive amount means that previous largest creditor still needs to receive some money
+            // instead negative amount means that previous largest debitor still needs to give some money
+            int256 toAdjust = largestCreditor.amount + largestDebitor.amount;
+            if (toAdjust < 0) {
+                // Debitor still owes money, push back with updated amount
+                BalanceHeap.insert(debitors, Balance(largestDebitor.member, toAdjust));
+            } else if (largestDebitorAmount < largestCreditorAmount) {
+                // Creditor still needs to receive money, push back with updated amount
+                BalanceHeap.insert(creditors, Balance(largestCreditor.member, toAdjust));
+            }
+        }
+        emit DebtSimplified(group.id);
     }
 
     function abs(int256 x) internal pure returns (uint256) {
@@ -89,43 +78,5 @@ library DebtSimplifier {
                 group.debts[members.at(j)][members.at(i)] = 0;
             }
         }
-    }
-
-    function selectionSortOrder(Balance[] memory balances) private pure {}
-
-    function insertionSort(
-        Balance[] memory balances,
-        uint256 realSize
-    ) private pure {
-        for (uint i = 1; i < realSize; i++) {
-            Balance memory toInsert = balances[i];
-            uint correctPos = i - 1;
-            while (
-                correctPos > 0 && balances[correctPos].amount > toInsert.amount
-            ) {
-                balances[correctPos + 1] = balances[correctPos];
-                correctPos -= 1;
-            }
-            balances[correctPos + 1] = toInsert;
-        }
-    }
-
-    function correctLastElementPosition(
-        Balance[] memory balances,
-        uint256 realSize
-    ) private pure {
-        if (realSize == 1) {
-            return;
-        }
-        uint currentElemPos = realSize - 1;
-        Balance memory toInsert = balances[currentElemPos];
-        uint correctPos = currentElemPos - 1;
-        while (
-            correctPos > 0 && balances[correctPos].amount > toInsert.amount
-        ) {
-            balances[correctPos + 1] = balances[correctPos];
-            correctPos -= 1;
-        }
-        balances[correctPos + 1] = toInsert;
     }
 }
